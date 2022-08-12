83e17cbbdddd6aad84db25ef23f123fec41bbe79
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-08-11 21:52:48 -0300

    feat: O nav, menu sandwich e o header mobile foram adicionados

---
+++ main.js
@@ -0,0 +1,14 @@
+const navigation = document.getElementById('navigation');
+
+function onScroll() {
+  scrollY > 0
+    ? navigation.classList.add('scroll')
+    : navigation.classList.remove('scroll');
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded');
+}
+function closeMenu() {
+  document.body.classList.remove('menu-expanded');
+}
